813eb902bf8b76208ac405ceb04c2e58f1f5cbb3
navvbrar <132538255+navvbrar@users.noreply.github.com>
2024-09-10 19:31:33 -0400

    chnaged api country

--- src/App.js
+++ src/App.js
@@ -32,13 +32,13 @@ const App =()=> {
         progress={loading}
   />
         <Switch>
-          <Route exact path="/"><News apikey={apikey}  setprogress={ setprogress } key="general" pagesize= "8" country="in" category="general"/></Route>
-          <Route exact path="/business"><News apikey={apikey}  setprogress={ setprogress } key="business" pagesize= "8" country="in" category="business"/></Route>
-          <Route exact path="/health"><News apikey={apikey}  setprogress={ setprogress  } key="health" pagesize= "8" country="in" category="health"/></Route>
+          <Route exact path="/"><News apikey={apikey}  setprogress={ setprogress } key="general" pagesize= "8" country="us" category="general"/></Route>
+          <Route exact path="/business"><News apikey={apikey}  setprogress={ setprogress } key="business" pagesize= "8" country="us" category="business"/></Route>
+          <Route exact path="/health"><News apikey={apikey}  setprogress={ setprogress  } key="health" pagesize= "8" country="us" category="health"/></Route>
           <Route exact path="/entertainment"><News apikey={apikey}  setprogress={ setprogress } key="entertainment" pagesize= "8" country="in" category="entertainment"/></Route>
-          <Route exact path="/sports"><News apikey={apikey}  setprogress={ setprogress } key="sports" pagesize= "8" country="in" category="sports"/></Route>
-          <Route exact path="/science"><News apikey={apikey}  setprogress={ setprogress } key="science" pagesize= "8" country="in" category="science"/></Route>
-          <Route exact path="/technology"><News apikey={apikey}  setprogress={ setprogress } key="technology" pagesize= "8" country="in" category="technology"/></Route>
+          <Route exact path="/sports"><News apikey={apikey}  setprogress={ setprogress } key="sports" pagesize= "8" country="us" category="sports"/></Route>
+          <Route exact path="/science"><News apikey={apikey}  setprogress={ setprogress } key="science" pagesize= "8" country="us" category="science"/></Route>
+          <Route exact path="/technology"><News apikey={apikey}  setprogress={ setprogress } key="technology" pagesize= "8" country="us" category="technology"/></Route>
         </Switch>
         </Router>
       </div> 
